308599f66249c816e5c625d981cc29cd28b910e3
beatrizbramont <beatriz.bramont@gmail.com>
2025-05-15 08:56:21 -0300

    fix: app - Atualização

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from database import db
+from config import app, db
 from controller.reserva_route import routes
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
 app.register_blueprint(routes)
 
 with app.app_context():
